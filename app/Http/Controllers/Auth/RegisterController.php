6cfe9f2c39f730979a8b91f6626390de98d614a6
urayusuke <crfnf690@yahoo.co.jp>
2020-08-18 20:37:15 +0900

    index update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,4 +70,10 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    public function redirectPath()
+    {
+        return '/';
+    }
+    
 }
--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,11 +1,21 @@
 <html>
   <head>@yield('title')</head>
   <link href="{{ asset('/scss/book/index.scss') }}" rel="stylesheet">
+  <style>
+    .booksRegistration{text-align: center; font-weight: bold; font-size: 18px;
+      letter-spacing: 1px;
+    }
+  </style>
 </html>
 <body>
 @extends('layouts.app')
 
 @section('content')
+  @if (Auth::check())
+    <div class="booksRegistration">
+      <a href="/">本を登録する</a>
+    </div>
+  @endif
   <div class="content">
     <ul>
       <li></li>
