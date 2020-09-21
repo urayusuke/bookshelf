851f798acd0b428e01dd4837459e12d7f8c9efc9
urayusuke <crfnf690@yahoo.co.jp>
2020-09-21 16:15:17 +0900

    web update

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('book.index');
-// });
-
 Route::get('/','BooksController@index');
 
 Auth::routes();
