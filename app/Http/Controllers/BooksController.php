d20aece596afc61999395a3c2aae10a6c1d760af
urayusuke <crfnf690@yahoo.co.jp>
2020-09-23 19:03:39 +0900

    env update

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -23,11 +23,11 @@ class BooksController extends Controller
             $sort = $request->sort;
         }
         $items = Book::orderBy($sort,'asc')->simplePaginate(5);
-        if(count($items)===0){
-            return view('book.index',['items' => $items]);
-        }else{
+        // if(count($items)===0){
+            // return view('book.index');
+        // }else{
             return view('book.index',['items' => $items, 'sort' => $sort]);
-        }
+        // }
     }
 
     /**
